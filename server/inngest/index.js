9059c945874ba81ef1d87f8b5f0e691393f7002b
Shambhavi Mishra <shambhavim1202@gmail.com>
2026-03-28 18:07:37 +0530

    updated server and index

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,13 +1,14 @@
 import { Inngest } from "inngest";
-import User from "../models/User";
+import User from "../models/User.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "wannachat-app" });
 
 //Inngest function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
+ {   id: 'sync-user-from-clerk',
+    triggers: [{event: 'clerk/user.created'}],
+  },
     async({event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         let username = email_addresses[0].email_addresses.split('@')[0]
@@ -32,8 +33,10 @@ const syncUserCreation = inngest.createFunction(
 
 //Inngest function to update user data in database
 const syncUserUpdation = inngest.createFunction(
-    {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated'},
+ {   
+    id: 'update-user-from-clerk',
+    triggers: [{event: 'clerk/user.updated'}],
+ },
     async({event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         
@@ -48,8 +51,10 @@ const syncUserUpdation = inngest.createFunction(
 
 //Inngest function to delete user data in database
 const syncUserDeletion = inngest.createFunction(
-    {id: 'delete-user-from-clerk'},
-    {event: 'clerk/user.deleted'},
+   {
+     id: 'delete-user-from-clerk',
+    triggers: [{event: 'clerk/user.deleted'}],
+   },
     async({event})=>{
         const {id} = event.data
         await User.findByIdAndDelete(id)
