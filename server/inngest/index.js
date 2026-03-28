c0bd611d4a0e3f37f7f86ef66b4d1080a98aabc6
Shambhavi Mishra <shambhavim1202@gmail.com>
2026-03-28 18:22:24 +0530

    updated index

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -11,7 +11,13 @@ const syncUserCreation = inngest.createFunction(
   },
     async({event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data
-        let username = email_addresses[0].email_addresses.split('@')[0]
+        const email = email_addresses?.[0]?.email_address
+
+         if (!email) {
+              throw new Error("Email not found in Clerk event")  
+              } 
+
+           let username = email.split('@')[0]
 
         //Check availabilty of username
         const user = await User.findOne({username})
